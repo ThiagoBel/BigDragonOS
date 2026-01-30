ad6a3bd7e883842ce81e72584c8919796a3d6193
Thiago Bel <narbnooxiinn@gmail.com>
2026-01-30 19:52:21 -0300

    Add files via upload

---
+++ storage/apps/INFO.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "INFORMACOES DO OPEN_APPS" << std::endl;
+    std::cout << "Use 'ESC' para sair de qualquer aplicativo." << std::endl;
+    std::cout << "Ele executa APENAS arquivos .cpp, nada mais." << std::endl;
+    std::cout << "Necessario do aplicativo MinGW (Do windows mesmo)" << std::endl;
+    return 0;
+}
