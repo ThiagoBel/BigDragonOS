d43b5ec30e85ef355f08c2d7ffaba33299858dc6
Thiago Bel <narbnooxiinn@gmail.com>
2026-01-31 19:45:39 -0300

    Delete openterminaltest.bat

--- openterminaltest.bat
+++
@@ -1,3 +0,0 @@
-windres storage/configs/sub_configs/bdOS.rc -O coff -o storage/configs/sub_configs/bdOS_res.o
-g++ bdOS.cpp storage/configs/sub_configs/bdOS_res.o -o bdOS.exe -lwinmm
-start bdOS.exe
